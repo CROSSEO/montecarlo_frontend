74f54e70a22638a354982bacebea13622f8d8ef2
Sebastian <sebastian.cheregi@hotmail.com>
2021-03-24 18:15:43 +0000

    commit to master

---
+++ src/constants/menu.js
@@ -0,0 +1,32 @@
+import { adminRoot } from './defaultValues';
+
+const data = [
+  {
+    id: 'dashboard',
+    icon: 'iconsminds-shop-4',
+    label: 'menu.dashboard',
+    to: `${adminRoot}/gogo`,
+    subs: [
+      {
+        icon: 'simple-icon-paper-plane',
+        label: 'menu.start',
+        to: `${adminRoot}/gogo/start`,
+      },
+    ],
+  },
+  {
+    id: 'secondmenu',
+    icon: 'iconsminds-three-arrow-fork',
+    label: 'menu.second-menu',
+    to: `${adminRoot}/second-menu`,
+    // roles: [UserRole.Admin, UserRole.Editor],
+    subs: [
+      {
+        icon: 'simple-icon-paper-plane',
+        label: 'menu.second',
+        to: `${adminRoot}/second-menu/second`,
+      },
+    ],
+  },
+];
+export default data;
